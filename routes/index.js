97583d3c0366766098912277e1169fc0dc338506
Gurjit Kaur <kgurjit@gmail.com>
2016-10-10 23:13:49 -0400

    1. Read before/after image from data 2. Basic contractors page with react components

--- routes/index.js
+++ routes/index.js
@@ -9,14 +9,26 @@ router.get('/', function(req, res, next) {
 router.get('/category/:catgName', function(req, res, next){
 	//TODO: Populate data from database
 	var data = {};
-	data.name = req.params.catgName;
-	data.contractors = [];
-	data.videos = [];
+	switch (catgName){
+		case 'kitchen':
+			data.name = req.params.catgName;
+			data.contractors = [];
+			data.videos = [];
+			data.images = {before: 'big_2.jpg', after: 'big_1.jpg'};
+			break;
+		case 'bathroom':
+			data.name = req.params.catgName;
+			data.contractors = [];
+			data.videos = [];
+			data.images = {before: 'big_2.jpg', after: 'big_1.jpg'};
+			break;
+	}
+	 
 	res.render('category', {'data': data});
 });
 
 router.get('/contractors', function(req, res, next){
-	res.render('contractors', {name: 'Contractors'});
+	res.render('contractors', {name: 'Contractors', js: ['contractors.js']});
 });
 
 router.get('/designers', function(req, res, next){
